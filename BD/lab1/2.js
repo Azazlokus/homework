87955de2f3e102319b4cb3bf8693c780d3332032
Yondu715 <maximvorobev57@gmail.com>
2022-12-15 01:47:33 +0700

    sw

--- BD/lab1/2.js
+++
@@ -1,7 +0,0 @@
-let lessons = ["Базы данных", "Введение в машинное обучение", "Струтуры и алгоритмы обработки данных"]
-let list = document.getElementById("lst")
-for (let i = 0; i < lessons.length; i++) {
-	let row = document.createElement("li")
-	row.textContent = lessons[i]
-	list.appendChild(row)
-}
